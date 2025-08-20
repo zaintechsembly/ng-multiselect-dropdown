e3b7cd8fd33791f13e620461f049fd73598388d8
Toqeer Abbas <toqeer@techsembly.com>
2025-08-20 14:01:49 +0500

    remove test files

--- src/ng-multiselect-dropdown/src/public_api.ts
+++ src/ng-multiselect-dropdown/src/public_api.ts
@@ -1,3 +1,5 @@
 export { MultiSelectComponent } from './multiselect.component';
 export { NgMultiSelectDropDownModule } from './ng-multiselect-dropdown.module';
 export { IDropdownSettings } from './multiselect.model';
+export { ListFilterPipe } from './list-filter.pipe';
+export { ClickOutsideDirective } from './click-outside.directive';
